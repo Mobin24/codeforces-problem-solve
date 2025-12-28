803c589387d205f6d0fe0af13cde04d4e16e9d01
Md Abdullah All Mobin <mdabdullahallmobin00@gmail.com>
2025-12-28 22:49:03 +0600

    Add files via upload

---
+++ 1676A.cpp
@@ -0,0 +1,27 @@
+//https://codeforces.com/contest/1676/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+        int t;
+        cin>>t;
+        while (t--)
+        {
+            char s[7];
+            cin>>s;
+            int frst=((s[0]-48)+(s[1]-48)+(s[2]-48));
+            int lst=((s[3]-48)+(s[4]-48)+(s[5]-48));
+
+            if(frst==lst){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+
+        }
+
+        
+}
