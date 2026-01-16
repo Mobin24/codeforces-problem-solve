d5bd640cf2b10ab54d9265e7bca3994c47754345
Md Abdullah All Mobin <mdabdullahallmobin00@gmail.com>
2026-01-16 12:33:01 +0600

    Add files via upload

---
+++ 2179A.cpp
@@ -0,0 +1,18 @@
+//https://codeforces.com/contest/2179/problem/A
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--) {
+        int a,b;
+        cin>>a>>b;
+        cout<<(a*b)+1<<endl;
+
+    }
+    return 0;
+}
