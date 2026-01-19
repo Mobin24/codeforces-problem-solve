976a24ed5d77b457ee07105377f4645e2e9152ef
Md Abdullah All Mobin <mdabdullahallmobin00@gmail.com>
2026-01-19 12:49:43 +0600

    Add solution for Codeforces contest 2185B
    Implement solution for Codeforces problem 2185B.

---
+++ 2185B.cpp
@@ -0,0 +1,24 @@
+//https://codeforces.com/contest/2185/problem/B
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+           
+        }
+         cout<<n*(*max_element(arr.begin(),arr.end()))<<endl;
+ 
+}
+ 
+}
