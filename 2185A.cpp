d838fee39f5228536c18c0283769f6334da4e1b3
Md Abdullah All Mobin <mdabdullahallmobin00@gmail.com>
2026-01-19 12:48:09 +0600

    Add solution for Codeforces problem 2185A

---
+++ 2185A.cpp
@@ -0,0 +1,27 @@
+//https://codeforces.com/contest/2185/problem/A
+
+
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+      int n;
+      cin>>n;
+      for(int i=1;i<=n;i++)
+      {
+        cout<<i<<" ";
+      }
+      cout<<endl;
+    }
+ 
+    return 0;
+
+}
