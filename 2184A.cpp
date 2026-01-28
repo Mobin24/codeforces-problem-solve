f59cf3847e6b5460ce2b0c7f7af8d3c794bcdbb2
Mobin24 <mdabdullahallmobin00@gmail.com>
2026-01-28 20:43:04 +0600

    Add new Codeforces solution

---
+++ 2184A.cpp
@@ -0,0 +1,24 @@
+//https://codeforces.com/contest/2184/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin>>n;
+        if(n<=3)
+        {
+            cout<<n<<endl;
+        }
+        else{
+            cout<<n%2<<endl;
+        }
+ 
+}
+ 
+}
