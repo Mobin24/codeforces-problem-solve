fce83814694d0e86da2b4ac9f9e52d9ff1e74f01
Md Abdullah All Mobin <mdabdullahallmobin00@gmail.com>
2026-02-18 10:27:15 +0600

    Add files via upload

---
+++ 1870A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+         int n,k,x;
+         cin>>n>>k>>x;
+         if(k>n || x<k-1)
+         {
+            cout<<-1<<endl;
+         }
+         else{
+            int ans=k*(k-1)/2;
+            if(k==x)x--;
+            ans+=x*(n-k);
+            cout<<ans<<endl;
+         }
+    }
+    return 0;
+}
